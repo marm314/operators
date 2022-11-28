e2242f280bbaf2117ab4a61c8c364fcd4699f45e
M. Rodriguez-Mayorga <marm3.14@gmail.com>
2022-11-28 16:54:04 +0100

    Read argument commutator.cpp

--- commutators_tmp.cpp
+++ commutators_tmp.cpp
@@ -6,9 +6,43 @@ using namespace std;
 string path="PATH";
 string command;
 
-int main()
-{ 
+int main(int argc, char *argv[])
+{
+ cout<<"--------------------------------------------"<<endl; 
+ cout<<"--------------------------------------------"<<endl; 
+ cout<<"---          COMMUTATORS PROGRAM         ---"<<endl; 
+ cout<<"--------------------------------------------"<<endl; 
+ cout<<"--------------------------------------------"<<endl; 
+ cout<<"-- Developed by: Dr. M. Rodriguez-Mayorga --"<<endl; 
+ cout<<"--     email: marm3.14@gmail.com          --"<<endl; 
+ cout<<"--------------------------------------------"<<endl; 
+ cout<<"--------------------------------------------"<<endl;
+ cout<<endl; 
+ string input_file; 
  command=path+"/op_strings";
- cout<<command<<endl; 
+ if(argc!=2)
+ {
+  cout<<"Include the name of the input file"<<endl;
+  cout<<"e.g."<<endl;
+  cout<<"./commutators name.inp"<<endl;
+  // Terminate screen output
+  cout<<endl; 
+  cout<<"--------------------------------------------"<<endl; 
+  cout<<"--            Normal termination          --"<<endl;
+  cout<<"--------------------------------------------"<<endl; 
+  cout<<"--------------------------------------------"<<endl; 
+  return 0;
+ }
+ else
+ {
+  input_file=argv[1];
+ }
+ 
+ // Terminate screen output
+ cout<<endl; 
+ cout<<"--------------------------------------------"<<endl; 
+ cout<<"--            Normal termination          --"<<endl;
+ cout<<"--------------------------------------------"<<endl; 
+ cout<<"--------------------------------------------"<<endl; 
  return 0;
 }
