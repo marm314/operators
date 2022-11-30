c747cb57c6d6cabd8bf509f3c9f710cafb46ca60
M. Rodriguez-Mayorga <marm3.14@gmail.com>
2022-11-30 10:19:45 +0100

    Init opr and corfs ints code

--- Makefile
+++ Makefile
@@ -2,9 +2,11 @@ all:
 	g++ -Wall -O3 String_ops.cpp op_strings.cpp -o op_strings
 	./path.sh
 	g++ -Wall -O3 commutators.cpp -o commutators
+	g++ -Wall -O3 integrals_coefs.cpp -o integrals_coefs
 clean:
 	/bin/rm -rf op_strings
 	/bin/rm -rf commutators.cpp 
 	/bin/rm -rf commutators
+	/bin/rm -rf integrals_coefs
 tar:
 	tar -cvf operators.tar *cpp *h Makefile test *sh
---
+++ integrals_coefs.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<algorithm>
+#include<fstream>
+#include<cstring>
+#include<vector>
+#include<stdlib.h>
+#include<stdio.h>
+#include<iomanip>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+ ifstream file_reader;
+ cout<<"--------------------------------------------"<<endl; 
+ cout<<"--------------------------------------------"<<endl; 
+ cout<<"---    INT, COEF & OPERATOS PROGRAM      ---"<<endl; 
+ cout<<"--------------------------------------------"<<endl; 
+ cout<<"--------------------------------------------"<<endl; 
+ cout<<"-- Developed by: Dr. M. Rodriguez-Mayorga --"<<endl; 
+ cout<<"--     email: marm3.14@gmail.com          --"<<endl; 
+ cout<<"--------------------------------------------"<<endl; 
+ cout<<"--------------------------------------------"<<endl;
+ cout<<endl; 
+ if(argc!=2)
+ {
+  cout<<"Include the name of the input file"<<endl;
+  cout<<"e.g."<<endl;
+  cout<<"./integrals_coefs name.inp"<<endl;
+  // Terminate screen output
+  cout<<endl; 
+  cout<<"--------------------------------------------"<<endl; 
+  cout<<"--            Normal termination          --"<<endl;
+  cout<<"---    INT, COEF & OPERATOS PROGRAM      ---"<<endl; 
+  cout<<"--------------------------------------------"<<endl; 
+  cout<<"--------------------------------------------"<<endl; 
+  return 0;
+ }
+ else
+ {
+  
+ }
+ // Terminate screen output
+ cout<<endl; 
+ cout<<"--------------------------------------------"<<endl; 
+ cout<<"--            Normal termination          --"<<endl;
+ cout<<"---    INT, COEF & OPERATOS PROGRAM      ---"<<endl; 
+ cout<<"--------------------------------------------"<<endl; 
+ cout<<"--------------------------------------------"<<endl; 
+ return 0;
+}
